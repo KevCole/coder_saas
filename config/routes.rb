2a15cd64b00838d7cdd6de173ae6987df26785dd
KevCole <Kevincolejr@outlook.com>
2015-09-30 15:57:21 +0000

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   
+  get '/about'=>'pages#about'
+  
+  
+  
+  
+  
+  
   root 'pages#home'
   
   # The priority is based upon order of creation: first created -> highest priority.
